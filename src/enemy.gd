f06a80458904f3aef94e68b094a647901eb33104
mdechdee <dechdee.m@gmail.com>
2020-01-25 23:07:04 +0700

    First enemy prototype
    2 red penguins walking back and forth with the same movement mechanic as the player

--- src/enemy.gd
+++ src/enemy.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-export(int) var slideness = 10
-export(int) var speed = 2;
+export(int) var slideness = 0.2
+export(int) var speed = 280;
 
 var velocity:Vector2 = Vector2()
 var up:Vector2 = Vector2(0, -1)
@@ -9,31 +9,41 @@ var gravityScale = 1200
 enum Direction {L=-1, R=1}
 
 var currentDirection:int = Direction.L
-onready var rayCast1: RayCast2D = $rayCast1
-onready var rayCast2: RayCast2D = $rayCast2
+onready var raycast1: RayCast2D = $rayCast1
+onready var raycast2: RayCast2D = $rayCast2
 
 func _physics_process(delta):
-	velocity.x = speed * currentDirection
-	velocity.y += gravityScale
-	velocity += slide();
-	if !rayCast1.is_colliding() or !rayCast2.is_colliding():
-		currentDirection *= -1
+	velocity.y += gravityScale * delta
+	velocity.x = currentDirection * speed
+	velocity += slide()
+	
+	# If left hand is a hole, then go right
+	if !raycast1.is_colliding():
+		currentDirection = Direction.R
+		print(currentDirection)
+	# If right hand is a hole, then go left
+	if !raycast2.is_colliding():
+		currentDirection = Direction.L
+		print(currentDirection)
 	velocity = move_and_slide(velocity, up)
 
+# Slide algorithm, basically the same with player.gd
+# Consider making a component to make all things slide the same way (obstacles, enemies, player)?
 func slide():
-	var ground1:Node2D  = rayCast1.get_collider()
-	var ground2:Node2D  = rayCast1.get_collider()
-	var rotationDegrees = 0
+	var ground1 = raycast1.get_collider()
+	var ground2 = raycast2.get_collider()
+	var rotationDegrees = 0.0
 	var divideBy = 0
-	if ground1 != null or ground2 != null:
-		if ground1 != null:
-			rotationDegrees += ground1.rotation_degrees
-			divideBy += 1
-		if ground2 != null:
-			rotationDegrees += ground2.rotation_degrees
-			divideBy += 1
-	else:
-		return Vector2(0, 0)
+	if ground1!=null :
+		rotationDegrees += ground1.rotation_degrees
+		divideBy+=1;
+		pass
+	if ground1!=null :
+		rotationDegrees += ground1.rotation_degrees
+		divideBy+=1
+		pass
+	if rotationDegrees==0 :
+		return Vector2()
 	rotationDegrees = rotationDegrees / divideBy
 	rotation_degrees = rotationDegrees
-	return Vector2(1, 1) * (rad2deg(rotationDegrees) * slideness)
+	return (Vector2(1,1)* ( rad2deg(rotationDegrees)* slideness ))
--- src/player.gd
+++ src/player.gd
@@ -1,4 +1,3 @@
-
 extends Node
 
 var body;
