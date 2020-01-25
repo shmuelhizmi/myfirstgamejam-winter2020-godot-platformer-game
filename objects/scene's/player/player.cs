827cb947b038710666655f1ff9329de0dd0a3813
shmuelhizmi <shmuelhizmi1@gmail.com>
2020-01-25 14:04:36 +0200

    signels for gdscript added

--- objects/scene's/player/player.cs
+++ objects/scene's/player/player.cs
@@ -8,17 +8,32 @@ public class player : Node2D
     RayCast2D rayCast1;
     RayCast2D rayCast2;
     AnimationPlayer animationPlayer;
+
     //movement properties
     [Export] int speed = 280;
+    [Export] float runningMultiplier = 1.3f;
     [Export] int jumpForce = 450;
     [Export] int gravityScale = 1200;
     [Export] int slideness = 10;
+    [Export] public string idleAnimationName;
+    [Export] public string walkAnimationName;
+    [Export] public string runAnimationName;
+    [Export] public string jumpAnimationName;
+    [Export] public string crouchAnimationName;
+
+    //extends functions
+    [Signal] public delegate void _AfterProcess(float delta);
+    [Signal] public delegate void _BeforeProcess(float delta);
+    [Signal] public delegate void _AfterReady(float delta);
+    [Signal] public delegate void _AfterJump(float force);
+    [Signal] public delegate void _AfterMove(float force);
+    [Signal] public delegate void _onCollision(KinematicCollision2D collision);
+
 
 
     //movement variables
     Vector2 velocity = new Vector2();
-    Vector2 up = new Vector2(0, -1);
-    bool isJumping = true;
+    Vector2 up = new Vector2(0, -1); // for collision testing
     public override void _Ready()
     {
         //get nodes
@@ -27,29 +42,41 @@ public class player : Node2D
         rayCast1 = body.GetNode<RayCast2D>("rayCast1");
         rayCast2 = body.GetNode<RayCast2D>("rayCast2");
 
+        EmitSignal("_AfterReady");
+
     }
 
     public override void _Process(float delta)
     {
-        this.handleInput();
-        velocity.y += gravityScale * delta;
-        if (isJumping && body.IsOnFloor())
-        {
-            isJumping = false;
-        }
+        EmitSignal("_BeforeProcess", delta);
+
+        this.handleInput();// get velocity from input
+
+        velocity.y += gravityScale * delta; //apply grvity 
         velocity += slide() * delta;
+
+        if (velocity.x == 0)// is player is idle
+        {
+            setAnimation(idleAnimationName);
+        }
         velocity = body.MoveAndSlide(velocity, up);
         KinematicCollision2D collision = body.MoveAndCollide(new Vector2());
+        if (collision != null)
+        {
+            EmitSignal("_onCollision", collision);
+        }
+        EmitSignal("_AfterProcess", delta);
     }
 
     public void handleInput()
     {
-        if (Input.IsKeyPressed(16777217))
+        if (Input.IsKeyPressed(16777217))// if esc pressed exit
         {
             GetTree().Quit();
         }
         move();
     }
+
     public Vector2 slide()
     {
         Node2D ground1 = (Node2D)rayCast1.GetCollider();
@@ -61,13 +88,11 @@ public class player : Node2D
             if (ground1 != null)
             {
                 rotationDegrees += ground1.RotationDegrees;
-                GD.Print(rotationDegrees);
                 divideBy++;
             }
             if (ground2 != null)
             {
                 rotationDegrees += ground2.RotationDegrees;
-                GD.Print(rotationDegrees);
                 divideBy++;
             }
         }
@@ -78,7 +103,6 @@ public class player : Node2D
 
         rotationDegrees = rotationDegrees / divideBy;
         body.RotationDegrees = rotationDegrees;
-        GD.Print((new Vector2(1, 1) * (Mathf.Rad2Deg(rotationDegrees) * slideness)));
         return new Vector2(1, 1) * (Mathf.Rad2Deg(rotationDegrees) * slideness); ;
     }
 
@@ -87,6 +111,7 @@ public class player : Node2D
         bool right = Input.IsActionPressed("player_right");
         bool left = Input.IsActionPressed("player_left");
         bool jump = Input.IsActionPressed("player_jump");
+        bool run = Input.IsActionPressed("player_run");
         bool isonfloor = body.IsOnFloor();
 
         velocity.x = 0;
@@ -97,24 +122,46 @@ public class player : Node2D
         }
         if (right)
         {
-            actionMove(Directin.right);
+            actionMove(Directin.right, run);
         }
 
         if (left)
         {
-            actionMove(Directin.left);
+            actionMove(Directin.left, run);
         }
         return;
 
     }
     void actionJump()
     {
+        if (jumpAnimationName != null)
+        {
+            animationPlayer.Play(jumpAnimationName);
+        }
         velocity.y = -jumpForce;
-        isJumping = true;
+        EmitSignal("_AfterJump", jumpForce);
     }
-    void actionMove(int directin)
+    void actionMove(int directin, bool isRunning)
     {
-        velocity.x += speed * directin;
+        if (isRunning)
+        {
+            velocity.x += speed * directin * runningMultiplier;
+            setAnimation(runAnimationName);
+        }
+        else
+        {
+            setAnimation(walkAnimationName);
+            velocity.x += speed * directin;
+        }
+
+        EmitSignal("_AfterMove", speed * directin);
+    }
+    public void setAnimation(string animation)
+    {
+        if (animation != null && animationPlayer.CurrentAnimation != animation)
+        {
+            animationPlayer.SetCurrentAnimation(animation);
+        }
     }
     struct Directin
     {
