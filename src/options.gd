81d1a851acb1c609f2b29a1a93b9fb654e517746
MAJD AR <zzmmzzmm32192@gmail.com>
2020-01-28 13:46:03 +0200

    working sound system

--- src/options.gd
+++ src/options.gd
@@ -1,21 +1,33 @@
 extends Control
 
+var load_v = false
 
 func _ready():
 	$AnimationPlayer/fade.visible = true
 	$AnimationPlayer/fade.modulate = Color(0, 0, 0, 1)
 	$AnimationPlayer.play("fade_in_options")
-
+#warning-ignore:standalone_expression
+	load_v = true
+	$music.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index('music'))
+	$sounds.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index('sounds'))
 
 func _on_music_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index('music'), value)
-	$music/music_volume.play()
+	if load_v == false:
+		$music/music_volume.play()
+
+
+
 
 func _on_sounds_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index('sounds'), value)
-	$sounds/music_volume.play()
+	if load_v == false:
+		$sounds/music_volume.play()
 
 
+
+
+#warning-ignore:unused_argument
 func _physics_process(delta):
 	if $music.value == 0:
 		$music/value.text =  '100%'
@@ -42,7 +54,9 @@ func _physics_process(delta):
 		$sounds/value.text =  '20%'
 	elif $sounds.value == -50:
 		$sounds/value.text =  '0%'
-
+	
+	if load_v == true:
+		load_v = false
 
 
 
@@ -52,4 +66,5 @@ func _on_back_pressed():
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == 'fade_out':
+#warning-ignore:return_value_discarded
 		get_tree().change_scene("res://objects/scene\'s/ui/start_screen.tscn")
